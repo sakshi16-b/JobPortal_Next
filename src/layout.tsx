a77a27c98dd8b3284e6c773901935a1e85963d22
sakshi16-b <sakshibisht016@gmail.com>
2026-05-22 10:56:15 +0530

    install sonner

--- src/layout.tsx
+++ src/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Montserrat, Urbanist } from "next/font/google";
 import "./globals.css";
+import { Toaster } from "./components/ui/sonner";
 
 const montserrat = Montserrat({
   subsets: ["latin"],
@@ -32,6 +33,7 @@ export default function RootLayout({
       <body
         className={`${montserrat.variable} ${urbanist.variable} font-sans antialiased`}
       >
+      <Toaster/>
         {children}
       </body>
     </html>
